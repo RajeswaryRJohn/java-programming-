0975aa4df44568fa5f37a31b1d8104f2f86412da
RajeswaryRJohn <43035153+RajeswaryRJohn@users.noreply.github.com>
2018-09-07 11:59:12 +0530

    Add files via upload

---
+++ Mathemetical operation.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a=1898689;
+    b=889756;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println(c+" "+d+" "+e+" "+f+" ");
+  }
+}
