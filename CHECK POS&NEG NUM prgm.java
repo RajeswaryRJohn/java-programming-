21105fc609f5b30c3af73ad9fc1bf3d92474d7f4
RajeswaryRJohn <43035153+RajeswaryRJohn@users.noreply.github.com>
2018-10-03 12:32:02 +0530

    Add files via upload

---
+++ CHECK POS&NEG NUM prgm.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+   int num;
+   num=3;
+   if(num>0)
+   {
+   System.out.println(num+" POSITIVE");
+  }
+   else if(num<0)
+   {
+   System.out.println(num+" NEGATIVE");
+   }
+   else
+   {
+   System.out.println(num+" ZERO");
+   }
+  }
+}
