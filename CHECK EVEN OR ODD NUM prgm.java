547e94c9add8c8d3ca20d5cb0c3ae6d9c05e1c39
RajeswaryRJohn <43035153+RajeswaryRJohn@users.noreply.github.com>
2018-10-03 12:39:42 +0530

    Add files via upload

---
+++ CHECK EVEN OR ODD NUM prgm.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+   int num;
+   num=37;
+    if(num%2==0)
+    {
+    System.out.println(num+" EVEN");
+    }
+    else if(num%2!=0)
+    {
+    System.out.println(num+" ODD");
+    }
+    else
+    {
+    System.out.println(num+" INVALID");
+    }
+  }
+}
